29cbc7423dc4c3b6e9772b5a20bf205b155c9a3d
Gonzalo Quinteros <chonz0@gmail.com>
2019-11-21 17:09:12 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const socket = require('socket.io')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const validator = require('express-validator')
+const app = express()
+const server = require('http').Server(app)
+const io = socket(server)
+
+const port = process.env.PORT || 3000
+const route = require('./app/routes')
+
+app.use(cors())
+app.use((req,res,next) => {
+  req.io = io
+  next()
+})
+app.use(bodyParser.json())
+app.use(validator())
+app.use(route)
+
+server.listen(port, () => {
+  console.log('Listening on '+port)
+})
+
+const random = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+setInterval(() => {
+  const stats = {
+    appointments: {
+      today: {
+        total: random(1, 1000),
+        completed: random(1, 1000),
+        pending: random(1, 1000),
+        canceled: random(1, 1000),
+      }
+    },
+    operators: {
+      active: random(1, 50),
+      inactive: random(1, 50),
+    },
+    subscriptions: {
+      total: random(1, 1000),
+      completed: random(1, 1000),
+      waiting: {
+        total: random(1, 1000),
+        checkedIn: random(1, 1000),
+        notCheckedIn: random(1, 1000),
+      }
+    },
+    channels: {
+      totem: random(1, 1000),
+      app: random(1, 1000),
+      entrance: random(1, 1000),
+    },
+    users: {
+      active: random(1, 1000),
+      inactive: random(1, 1000)
+    }
+  };
+
+  console.log('stats', stats);
+  io.emit('stats', stats);
+}, 1000);
